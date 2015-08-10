a4c5dbd6bbe447b4a9fb642aab4a9651aca12cea
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-08-11 01:43:48 +0530

    calendar

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -79,8 +79,8 @@ object Application extends Controller {
     val data = Json.obj(
       "attachments" -> Json.obj("fileUrl" -> "http://add2cal.herokuapp.com"),
       "attendees" -> Json.arr(Json.obj("email" -> "pamu2java@gmail.com")),
-      "start" -> Json.obj("date" -> "2015-08-11", "dateTime" -> "2015-08-11T09:00:00-07:00", "timeZone" -> "Asia/Calcutta"),
-      "end" -> Json.obj("date" -> "2015-08-11", "dateTime" -> "2015-08-11T17:00:00-07:00", "timeZone" -> "Asia/Calcutta"),
+      "start" -> Json.obj("date" -> JsNull, "dateTime" -> "2015-08-11T09:00:00-07:00", "timeZone" -> "Asia/Calcutta"),
+      "end" -> Json.obj("date" -> JsNull, "dateTime" -> "2015-08-11T17:00:00-07:00", "timeZone" -> "Asia/Calcutta"),
       "reminders" -> Json.obj("overrides" -> Json.arr(Json.obj("method" -> "email", "minutes" -> "5")))
     )
     request.post(data).map {
