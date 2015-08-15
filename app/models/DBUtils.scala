b86bf4ada3c8cf13180edebee0440b6633116e4c
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-08-15 05:33:57 +0530

    insert not exists

--- app/models/DBUtils.scala
+++ app/models/DBUtils.scala
@@ -8,7 +8,14 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
  * Created by pnagarjuna on 13/08/15.
  */
 object DBUtils {
-  def createUser(user: User): Future[Int] = DB.db.run(DB.users += user)
+  def insertIfNotExistsUser(user: User) = DB.users.forceInsertQuery {
+    val exists = (for (u <- DB.users if u.email === user.email) yield u).exists
+    val insert = (user.host, user.email, user.pass, None) <> (User.apply _ tupled, User.unapply)
+    for (u <- Query(insert) if !exists) yield u
+  }
+  def createUser(user: User): Future[Int] = {
+    DB.db.run(insertIfNotExistsUser(user))
+  }
   def fetchUser(email: String): Future[Option[User]]  = {
     val q = for(user <- DB.users.filter(_.email === email)) yield user
     DB.db.run(q.result).map(_.headOption)
@@ -18,7 +25,12 @@ object DBUtils {
                 refreshTime <- DB.refreshTimes.filter(_.userId === user.id)) yield refreshTime
     DB.db.run(q.result).map(_.headOption)
   }
-  def createRefreshTime(refreshTime: RefreshTime): Future[Int] = DB.db.run(DB.refreshTimes += refreshTime)
+  def insertIfNotExistsRT(rtime: RefreshTime) = DB.refreshTimes.forceInsertQuery {
+    val exists = (for (rt <- DB.refreshTimes if rt.userId === rtime.userId) yield rt).exists
+    val insert = (rtime.accessToken, rtime.refreshToken, rtime.refreshTime, rtime.refreshPeriod, rtime.userId, None) <> (RefreshTime.apply _ tupled, RefreshTime.unapply)
+    for (rt <- Query(insert) if !exists) yield rt
+  }
+  def createRefreshTime(refreshTime: RefreshTime): Future[Int] = DB.db.run(insertIfNotExistsRT(refreshTime))
   def fetchUsers: Future[Seq[(User, RefreshTime)]] = {
     val q = for(user <- DB.users;
                 refreshTime <- DB.refreshTimes.filter(_.userId === user.id)) yield (user, refreshTime)
